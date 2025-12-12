45f64fa72c463cb08189e1d59c56706fd135ed7b
Kum Brian Ihimbru <kumbrianihimbru@gmail.com>
2025-12-12 13:40:42 +0100

    refactor: Remove unused CatchStatus import
    No longer needed since we use c.isAvailable instead of
    comparing to CatchStatus.available enum value

--- lib/features/fisher/presentation/screens/home_screen.dart
+++ lib/features/fisher/presentation/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:siren_marketplace/core/constants/app_colors.dart';
 import 'package:siren_marketplace/core/domain/entities/catch.dart';
 import 'package:siren_marketplace/core/domain/entities/offer.dart';
